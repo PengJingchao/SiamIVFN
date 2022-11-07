2ae8ca24a2479e45843d7d158cf0aa5a36a6979d
PengJingchao <43465990+PengJingchao@users.noreply.github.com>
2022-11-07 16:32:56 +0800

    add load_image_rgbtfusion

--- videoanalyst/utils/__init__.py
+++ videoanalyst/utils/__init__.py
@@ -11,5 +11,5 @@ __all__ = [
     load_image, Registry, Timer, load_cfg, md5sum, merge_cfg_into_hps,
     complete_path_wt_root_in_cfg, ensure_dir, average_gradients,
     convert_numpy_to_tensor, convert_tensor_to_numpy, move_data_to_device,
-    unwrap_model, VideoWriter
+    unwrap_model, VideoWriter, load_image_rgbtfusion
 ]
